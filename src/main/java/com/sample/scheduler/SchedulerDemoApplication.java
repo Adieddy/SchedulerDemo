81dc11197da6ad12c64960388bb529bcd246d8e1
Aditya Gupta <Aditya.gupta20@gmail.com>
2019-06-25 21:07:05 +0530

    Update SchedulerDemoApplication.java

--- src/main/java/com/sample/scheduler/SchedulerDemoApplication.java
+++ src/main/java/com/sample/scheduler/SchedulerDemoApplication.java
@@ -2,7 +2,9 @@ package com.sample.scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,6 +12,13 @@ public class SchedulerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulerDemoApplication.class, args);
+	}
+	
+	@Bean
+	public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+		threadPoolTaskScheduler.setPoolSize(2);
+		return threadPoolTaskScheduler;
 	}
 
 }
